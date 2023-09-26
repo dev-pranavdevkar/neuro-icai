4553faaa96b174120ac382c5885a0f03e2491e51
Pranav Devkar <109267896+devpranavdevkar@users.noreply.github.com>
2023-09-26 12:34:13 +0530

    Vacancies API

--- resources/views/frontend/vacancies/viewVacancies.blade.php
+++ resources/views/frontend/vacancies/viewVacancies.blade.php
@@ -20,192 +20,65 @@
 
     <section class="home-about spad bg-light pt-5">
         <div class="container">
-            <div class="row d-flex justify-content-center">
-      
-                
-                <div class="col-lg-6 py-3 ">
+            @if (isset($vacancyDetails) && count($vacancyDetails) > 0)
+                <div class="">
+                   
+                    <div class="row d-flex justify-content-center">
+                        @foreach ($vacancyDetails as $vacancy)
+                            <div class="col-lg-6 py-3 ">
 
-                    <div class="card jobcard h-100">
+                                <div class="card jobcard h-100">
 
-                        <div class="card-body ">
-                            <h5 class="card-title">Article Assistant</h5>
-                            <h6 class="card-subtitle mb-2 ">Pravin N Jain & Associates</h6>
-                            <div class=" posted-details">
-                                <ul class="d-flex justify-content-start">
-                                    <li class="mr-5"><i class="fa fa-briefcase" aria-hidden="true"></i>01-03 Yrs</li>
-                                    <li class="mr-5"><i class="fa fa-inr" aria-hidden="true"></i>30K-50K</li>
-                                    <li class="mr-5"><i class="fa fa-map-marker" aria-hidden="true"></i>Pune</li>
-                                </ul>
-                                <div class="mt-2">
-                                    <ul class="d-flex justify-content-between">
-                                        <li class="d-flex"><i class="fa fa-location-arrow mt-1" aria-hidden="true"></i>
-                                           Pune Branch of WIRC of ICAI
-                                            ICAI Bhawan, Plot No. 8, Near Mahavir Electronics, Parshwanath Nagar, Bibwewadi, Pune - 411 037, Maharashtra, India.</li>
-                                       </ul>
+                                    <div class="card-body ">
+                                        <h5 class="card-title">{{ $vacancy['position'] }}</h5>
+                                        <h6 class="card-subtitle mb-2 ">{{ $vacancy['ca_firm_name'] }}</h6>
+                                        <div class=" posted-details">
+                                            <ul class="d-flex justify-content-start">
+                                                <li class="mr-5"><i class="fa fa-briefcase" aria-hidden="true"></i>{{ $vacancy['experience'] }}
+                                                    Yrs</li>
+                                                <li class="mr-5"><i class="fa fa-inr" aria-hidden="true"></i>30K-50K</li>
+                                                <li class="mr-5"><i class="fa fa-map-marker" aria-hidden="true"></i>{{ $vacancy['location_details']['city'] }}</li>
+                                            </ul>
+                                            <div class="mt-2">
+                                                <ul class="d-flex justify-content-between">
+                                                    <li class="d-flex"><i class="fa fa-location-arrow mt-1"
+                                                            aria-hidden="true"></i>
+                                                            {{ $vacancy['location_details']['address_line_1'] }}
+                                                            {{ $vacancy['location_details']['address_line_2'] }}
+                                                            {{ $vacancy['location_details']['city'] }}
+                                                            {{ $vacancy['location_details']['state'] }}-{{ $vacancy['location_details']['pincode'] }}
+                                                        
+                                                        </li>
+                                                </ul>
+                                            </div>
+                                            <p class="mt-2">We are looking for {{ $vacancy['position'] }}. The candidates will get
+                                                exposure in the field of Accounting, Statutory Audits, Internal Audits,
+                                                Income Tax,
+                                                GST, TDS, etc.</p>
+                                        </div>
+                                        <div class="d-flex justify-content-end align-items-center">
+                                        
+                                            <a href="#" class="btn btn-primary">Apply</a>
+                                        </div>
+
+                                    </div>
                                 </div>
-                                <p class="mt-2">We are looking for Articles Assistant. The candidates will get
-                                    exposure in the field of Accounting, Statutory Audits, Internal Audits, Income Tax,
-                                    GST, TDS, etc.</p>
-                            </div>
-                               <div class="d-flex justify-content-end align-items-center">
-                                {{-- <a class="readMoreUnderline" href="#">Read More</a> --}}
-                                <a href="#" class="btn btn-primary">Apply</a>
-                            </div>
 
-                        </div>
+                            </div>
+                        @endforeach
                     </div>
 
+                    
                 </div>
-                <div class="col-lg-6 py-3 ">
-
-                    <div class="card jobcard h-100">
-
-                        <div class="card-body ">
-                            <h5 class="card-title">Article Assistant</h5>
-                            <h6 class="card-subtitle mb-2 ">Pravin N Jain & Associates</h6>
-                            <div class=" posted-details">
-                                <ul class="d-flex justify-content-start">
-                                    <li class="mr-5"><i class="fa fa-briefcase" aria-hidden="true"></i>01-03 Yrs</li>
-                                    <li class="mr-5"><i class="fa fa-inr" aria-hidden="true"></i>30K-50K</li>
-                                    <li class="mr-5"><i class="fa fa-map-marker" aria-hidden="true"></i>Pune</li>
-                                </ul>
-                                <div class="mt-2">
-                                    <ul class="d-flex justify-content-between">
-                                        <li class="d-flex"><i class="fa fa-location-arrow mt-1" aria-hidden="true"></i>
-                                           Pune Branch of WIRC of ICAI
-                                            ICAI Bhawan, Plot No. 8, Near Mahavir Electronics, Parshwanath Nagar, Bibwewadi, Pune - 411 037, Maharashtra, India.</li>
-                                       </ul>
-                                </div>
-                                <p class="mt-2">We are looking for Articles Assistant. The candidates will get
-                                    exposure in the field of Accounting, Statutory Audits, Internal Audits, Income Tax,
-                                    GST, TDS, etc.</p>
-                            </div>
-                               <div class="d-flex justify-content-end align-items-center">
-                                {{-- <a class="readMoreUnderline" href="#">Read More</a> --}}
-                                <a href="#" class="btn btn-primary">Apply</a>
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-lg-6 py-3 ">
-
-                    <div class="card jobcard h-100">
-
-                        <div class="card-body ">
-                            <h5 class="card-title">Article Assistant</h5>
-                            <h6 class="card-subtitle mb-2 ">Pravin N Jain & Associates</h6>
-                            <div class=" posted-details">
-                                <ul class="d-flex justify-content-start">
-                                    <li class="mr-5"><i class="fa fa-briefcase" aria-hidden="true"></i>01-03 Yrs</li>
-                                    <li class="mr-5"><i class="fa fa-inr" aria-hidden="true"></i>30K-50K</li>
-                                    <li class="mr-5"><i class="fa fa-map-marker" aria-hidden="true"></i>Pune</li>
-                                </ul>
-                                <div class="mt-2">
-                                    <ul class="d-flex justify-content-between">
-                                        <li class="d-flex"><i class="fa fa-location-arrow mt-1" aria-hidden="true"></i>
-                                           Pune Branch of WIRC of ICAI
-                                            ICAI Bhawan, Plot No. 8, Near Mahavir Electronics, Parshwanath Nagar, Bibwewadi, Pune - 411 037, Maharashtra, India.</li>
-                                       </ul>
-                                </div>
-                                <p class="mt-2">We are looking for Articles Assistant. The candidates will get
-                                    exposure in the field of Accounting, Statutory Audits, Internal Audits, Income Tax,
-                                    GST, TDS, etc.</p>
-                            </div>
-                               <div class="d-flex justify-content-end align-items-center">
-                                {{-- <a class="readMoreUnderline" href="#">Read More</a> --}}
-                                <a href="#" class="btn btn-primary">Apply</a>
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-lg-6 py-3 ">
-
-                    <div class="card jobcard h-100">
-
-                        <div class="card-body ">
-                            <h5 class="card-title">Article Assistant</h5>
-                            <h6 class="card-subtitle mb-2 ">Pravin N Jain & Associates</h6>
-                            <div class=" posted-details">
-                                <ul class="d-flex justify-content-start">
-                                    <li class="mr-5"><i class="fa fa-briefcase" aria-hidden="true"></i>01-03 Yrs</li>
-                                    <li class="mr-5"><i class="fa fa-inr" aria-hidden="true"></i>30K-50K</li>
-                                    <li class="mr-5"><i class="fa fa-map-marker" aria-hidden="true"></i>Pune</li>
-                                </ul>
-                                <div class="mt-2">
-                                    <ul class="d-flex justify-content-between">
-                                        <li class="d-flex"><i class="fa fa-location-arrow mt-1" aria-hidden="true"></i>
-                                           Pune Branch of WIRC of ICAI
-                                            ICAI Bhawan, Plot No. 8, Near Mahavir Electronics, Parshwanath Nagar, Bibwewadi, Pune - 411 037, Maharashtra, India.</li>
-                                       </ul>
-                                </div>
-                                <p class="mt-2">We are looking for Articles Assistant. The candidates will get
-                                    exposure in the field of Accounting, Statutory Audits, Internal Audits, Income Tax,
-                                    GST, TDS, etc.</p>
-                            </div>
-                               <div class="d-flex justify-content-end align-items-center">
-                                {{-- <a class="readMoreUnderline" href="#">Read More</a> --}}
-                                <a href="#" class="btn btn-primary">Apply</a>
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-lg-6 py-3 ">
-
-                    <div class="card jobcard h-100">
-
-                        <div class="card-body ">
-                            <h5 class="card-title">Article Assistant</h5>
-                            <h6 class="card-subtitle mb-2 ">Pravin N Jain & Associates</h6>
-                            <div class=" posted-details">
-                                <ul class="d-flex justify-content-start">
-                                    <li class="mr-5"><i class="fa fa-briefcase" aria-hidden="true"></i>01-03 Yrs</li>
-                                    <li class="mr-5"><i class="fa fa-inr" aria-hidden="true"></i>30K-50K</li>
-                                    <li class="mr-5"><i class="fa fa-map-marker" aria-hidden="true"></i>Pune</li>
-                                </ul>
-                                <div class="mt-2">
-                                    <ul class="d-flex justify-content-between">
-                                        <li class="d-flex"><i class="fa fa-location-arrow mt-1" aria-hidden="true"></i>
-                                           Pune Branch of WIRC of ICAI
-                                            ICAI Bhawan, Plot No. 8, Near Mahavir Electronics, Parshwanath Nagar, Bibwewadi, Pune - 411 037, Maharashtra, India.</li>
-                                       </ul>
-                                </div>
-                                <p class="mt-2">We are looking for Articles Assistant. The candidates will get
-                                    exposure in the field of Accounting, Statutory Audits, Internal Audits, Income Tax,
-                                    GST, TDS, etc.</p>
-                            </div>
-                               <div class="d-flex justify-content-end align-items-center">
-                                {{-- <a class="readMoreUnderline" href="#">Read More</a> --}}
-                                <a href="#" class="btn btn-primary">Apply</a>
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-                
-               
-
-            </div>
-
-
-
-
-            <div class="py-4 d-flex justify-content-between">
-                <a href="#" class="primary-btn">Previous</a>
-                <a href="#" class="primary-btn">Next</a>
-            </div>
-
+            
+            @else
+                <h1>No Vacancies Available.</h1>
+            @endif
 
         </div>
 
 
 
-        
+
     </section>
 @endsection
